ee737b1e0bf76c6fe89526a186719f77d95e87da
Kawyn Furstoss <kawynsean@gmail.com>
2024-09-04 13:09:38 -1000

    Wrap AlbumsView in NavigationStack and Scope to destination and path reducers

--- MobileTakeHome/MobileTakeHome/Features/Albums/AlbumsView.swift
+++ MobileTakeHome/MobileTakeHome/Features/Albums/AlbumsView.swift
@@ -13,48 +13,55 @@ struct AlbumsView: View {
     @State var isBottomModalShowing: Bool = false
     
     var body: some View {
-        ZStack(alignment: .top) {
-            GeometryReader { geometry in
-                let width = geometry.size.width / 3
-                if store.isLoading {
-                    ProgressView("Loading...")
-                        .position(x: geometry.size.width / 2, y: geometry.size.height / 2)
-                } else {
-                    SquareGridView(items: store.albums.elements, columnWidth: width) { album in
-                        if let image = album.images.first?.imageLink {
-                            if let imageUrl = URL(string: image) {
-                                AsyncThumbnailView(url: imageUrl, width: width)
-                                    .onTapGesture {
-                                        withAnimation {
-                                            store.send(.albumTapped(album))
-                                            isBottomModalShowing = true
+        NavigationStack(path: $store.scope(state: \.path, action: \.path)) {
+            ZStack(alignment: .top) {
+                GeometryReader { geometry in
+                    let width = geometry.size.width / 3
+                    if store.isLoading {
+                        ProgressView("Loading...")
+                            .position(x: geometry.size.width / 2, y: geometry.size.height / 2)
+                    } else {
+                        SquareGridView(items: store.albums.elements, columnWidth: width) { album in
+                            if let image = album.images.first?.imageLink {
+                                if let imageUrl = URL(string: image) {
+                                    AsyncThumbnailView(url: imageUrl, width: width)
+                                        .onTapGesture {
+                                            withAnimation {
+                                                store.send(.albumTapped(album))
+                                                isBottomModalShowing = true
+                                            }
                                         }
-                                    }
+                                }
                             }
                         }
+                        
                     }
-                    
+                }
+                SearchBarView(onSearch: { queryText in
+                    store.send(.queryAlbums(queryText))
+                })
+                .padding(.top, Padding.extraLarge * 3)
+            }
+            .modifier(BottomModalModifier(
+                shouldDisplay: $isBottomModalShowing,
+                onDismissed: {
+                    withAnimation {
+                        isBottomModalShowing = false
+                    }
+                }
+            ) {
+                AlbumModal(album: store.selectedAlbum) {
+                    // TODO: Navigate to View Album
                 }
             }
-            
-            SearchBarView(onSearch: { queryText in
-                store.send(.queryAlbums(queryText))
-            })
-            .padding(.top, Padding.extraLarge * 3)
+            )
         }
-        .modifier(BottomModalModifier(
-            shouldDisplay: $isBottomModalShowing,
-            onDismissed: {
-                withAnimation {
-                    isBottomModalShowing = false
-                }
-            }
-        ) {
-            AlbumModal(album: store.selectedAlbum) {
-                // TODO: Navigate to View Album
-            }
+    destination: { store in
+        switch store.case {
+        case .albumGallery(let store):
+            AlbumGalleryView(store: store)
         }
-        )
+    }
     }
 }
 
