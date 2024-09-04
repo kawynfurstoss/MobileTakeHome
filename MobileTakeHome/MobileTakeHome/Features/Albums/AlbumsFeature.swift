e772392a2ae078978c9f0aaee3145e0928227547
Kawyn Furstoss <kawynsean@gmail.com>
2024-09-03 22:24:08 -1000

    Create placeholder AlbumsFeature and AlbumsView

---
+++ MobileTakeHome/MobileTakeHome/Features/Albums/AlbumsFeature.swift
@@ -0,0 +1,25 @@
+//
+//  AlbumsFeature.swift
+//  MobileTakeHome
+//
+//  Created by Kawyn Furstoss on 9/3/24.
+//
+
+import ComposableArchitecture
+
+@Reducer
+struct AlbumsFeature {
+    @ObservableState
+    struct State: Equatable {
+        var albums: [String] = ["foo", "bar"]
+    }
+    enum Action {
+    }
+    
+    var body: some ReducerOf<Self> {
+        Reduce<State, Action> { state, action in
+            switch action {
+            }
+        }
+    }
+}
---
+++ MobileTakeHome/MobileTakeHome/Features/Albums/AlbumsView.swift
@@ -0,0 +1,32 @@
+//
+//  AlbumsView.swift
+//  MobileTakeHome
+//
+//  Created by Kawyn Furstoss on 9/3/24.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct AlbumsView: View {
+    @Bindable var store: StoreOf<AlbumsFeature>
+
+    var body: some View {
+        VStack {
+            ForEach(store.albums, id: \.self) { album in
+                Text(album)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(8)
+                    .foregroundColor(.white)
+            }
+        }
+    }
+}
+
+#Preview {
+    AlbumsView(store: Store(
+        initialState: AlbumsFeature.State(),
+        reducer: { AlbumsFeature()}
+    ))
+}
