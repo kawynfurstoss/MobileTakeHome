9a9fbebdcb4231fc766ea66e8e9fabecbead1edb
Kawyn Furstoss <kawynsean@gmail.com>
2024-09-04 10:00:18 -1000

    Create padding constants in component library

---
+++ MobileTakeHome/MobileTakeHome/ComponentsPackage/Styling/Padding.swift
@@ -0,0 +1,16 @@
+//
+//  Padding.swift
+//  MobileTakeHome
+//
+//  Created by Kawyn Furstoss on 9/4/24.
+//
+
+import Foundation
+
+struct Padding {
+    static let none: CGFloat = 0.0
+    static let small: CGFloat = 8.0
+    static let medium: CGFloat = 16.0
+    static let large: CGFloat = 24.0
+    static let extraLarge: CGFloat = 32.0
+}
