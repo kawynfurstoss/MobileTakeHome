77825104b5f4c49efa03ab4bf8143c4113ec4b08
Kawyn Furstoss <kawynsean@gmail.com>
2024-09-03 19:31:36 -1000

    Initial commit

---
+++ MobileTakeHome/MobileTakeHome/MobileTakeHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileTakeHomeApp.swift
+//  MobileTakeHome
+//
+//  Created by Kawyn Furstoss on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MobileTakeHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
