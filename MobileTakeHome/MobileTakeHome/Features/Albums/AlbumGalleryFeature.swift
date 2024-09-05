a3d395181a33bf035289cedeb3d99664d494d7b1
Kawyn Furstoss <kawynsean@gmail.com>
2024-09-04 18:30:55 -1000

    Added ability to favorite images in AlbumGallery

--- MobileTakeHome/MobileTakeHome/Features/Albums/AlbumGalleryFeature.swift
+++ MobileTakeHome/MobileTakeHome/Features/Albums/AlbumGalleryFeature.swift
@@ -12,13 +12,24 @@ import ComposableArchitecture
 struct AlbumGalleryFeature {
   @ObservableState
   struct State: Equatable {
-    let album: Album
+    var album: Album
+    @Shared(.favorites) var favorites
   }
   enum Action {
+      case imageAddedToFavorites(String)
+      case imageRemovedFromFavorites(String)
   }
   var body: some ReducerOf<Self> {
     Reduce { state, action in
       switch action {
+      case let .imageAddedToFavorites(id):
+          state.favorites.imageFavorites.append(id)
+          return .none
+      case let .imageRemovedFromFavorites(id):
+          state.favorites.imageFavorites.removeAll(where: { str in
+          id == str
+          })
+          return .none
       }
     }
   }
--- MobileTakeHome/MobileTakeHome/Features/Albums/AlbumGalleryView.swift
+++ MobileTakeHome/MobileTakeHome/Features/Albums/AlbumGalleryView.swift
@@ -11,11 +11,34 @@ import ComposableArchitecture
 struct AlbumGalleryView: View {
     let store: StoreOf<AlbumGalleryFeature>
     
-  var body: some View {
-      VStack {
-          Text("\(store.state.album.title ?? "null")")
-              .font(.title)
-          Spacer()
-      }
-  }
+    @State var isFavorite: Bool = true
+    var body: some View {
+        VStack {
+            Text("\(store.state.album.title ?? "null")")
+                .font(.title)
+            GeometryReader { geometry in
+                ScrollView {
+                    VStack(spacing: 20) {
+                        ForEach(store.state.album.images) { image in
+                            VStack {
+                                if let imageUrl = URL(string: image.imageLink) {
+                                    AsyncThumbnailView(url: imageUrl, width: geometry.size.width)
+                                }
+                                HStack {
+                                    Spacer()
+                                    FavoriteButton(isFavorite: store.favorites.imageFavorites.contains(where: { str in image.id == str })) { isFavorited in
+                                        if isFavorited {
+                                            store.send(.imageAddedToFavorites(image.id))
+                                        } else {
+                                            store.send(.imageRemovedFromFavorites(image.id))
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
