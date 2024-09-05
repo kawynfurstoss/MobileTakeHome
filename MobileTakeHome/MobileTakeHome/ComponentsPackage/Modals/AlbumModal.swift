265df69a3b892393d35d16439da9143a6e826950
Kawyn Furstoss <kawynsean@gmail.com>
2024-09-04 18:29:32 -1000

    Added FavoriteButton to AlbumModal for favoriting albums

--- MobileTakeHome/MobileTakeHome/ComponentsPackage/Modals/AlbumModal.swift
+++ MobileTakeHome/MobileTakeHome/ComponentsPackage/Modals/AlbumModal.swift
@@ -9,35 +9,54 @@ import SwiftUI
 
 struct AlbumModal: View {
     var album: Album?
+    var isFavorite: Bool
     var onViewGalleryTapped: () -> Void
+    var onFavoriteTapped: (Bool) -> Void
     
     var body: some View {
         VStack(alignment: .leading) {
-            VStack(alignment: .leading) {
-                Text("Album Contains \(album!.images.count) images")
-                    .font(.subheadline)
-                    .padding(.vertical, 5)
-                    .padding(.horizontal, 10)
-                    .background(Color.blue.opacity(0.5))
-                    .foregroundColor(.white)
-                    .clipShape(Capsule())
-                Text(album?.title ?? "")
-                    .foregroundColor(.black)
-                    .font(.title)
-                    .lineLimit(3)
-                Text(album?.description ?? "")
-                    .foregroundColor(.black)
-                    .font(.title3)
-                    .lineLimit(4)
+            HStack(alignment: .top) {
+                VStack(alignment: .leading) {
+                    Text("Album Contains \(album!.images.count) images")
+                        .font(.subheadline)
+                        .padding(.vertical, 5)
+                        .padding(.horizontal, 10)
+                        .background(Color.blue.opacity(0.5))
+                        .foregroundColor(.white)
+                        .clipShape(Capsule())
+                    Text(album?.title ?? "")
+                        .frame(maxWidth: .infinity, maxHeight: .infinity) // Ensure the text can expand to fit the container
+                        .foregroundColor(.black)
+                        .multilineTextAlignment(.leading)
+                        .font(.title2)
+                        .lineLimit(nil)
+                        .background(Color.gray.opacity(0.2))
+                    if let description = album?.description {
+                        Text(description)
+                            .frame(maxWidth: .infinity, maxHeight: .infinity) // Ensure the text can expand to fit the container
+                            .background(Color.red.opacity(0.2))
+                            .foregroundColor(.black)
+                            .font(.title3)
+                            .lineLimit(4)
+                    }
+
+                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity) // Ensure the text can expand to fit the container
+                .background(Color.red.opacity(0.2))
+                Spacer()
+                FavoriteButton(isFavorite: isFavorite) { isFavorited in
+                    onFavoriteTapped(isFavorited)
+                }
             }
-            .padding(20)
-            Spacer()
+            .padding(.horizontal, 20)
+            .padding(.top, 20)
             VStack {
                 CapsuleButton(label: "View Album") {
                   onViewGalleryTapped()
                 }
                 .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
             }
+            .background(Color.green.opacity(0.2))
             .padding(.bottom, Padding.large)
         }
     }
