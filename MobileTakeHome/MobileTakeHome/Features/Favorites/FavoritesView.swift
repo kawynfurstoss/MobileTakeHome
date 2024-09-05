bbb59de118ef0d0b443b6e3effde7d3ab64073a0
Kawyn Furstoss <kawynsean@gmail.com>
2024-09-04 19:47:40 -1000

    Implemented Segmented Control and image grids in Favorites tab view

--- MobileTakeHome/MobileTakeHome/Features/Favorites/FavoritesView.swift
+++ MobileTakeHome/MobileTakeHome/Features/Favorites/FavoritesView.swift
@@ -10,12 +10,53 @@ import ComposableArchitecture
 
 struct FavoritesView: View {
     let store: StoreOf<FavoritesFeature>
+    @State private var selectedSegment = 0
+    let segments = ["Albums", "Images"]
     
-    @State var isFavorite: Bool = true
     var body: some View {
-        VStack {
-            Text("Favorites View")
-                .font(.title)
+        GeometryReader { geometry in
+            let width = geometry.size.width / 2
+            VStack {
+                Picker("Select a view", selection: $selectedSegment) {
+                    ForEach(0..<segments.count) { index in
+                        Text(segments[index]).tag(index)
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+                
+                // View Content Based on Selection
+                if selectedSegment == 0 {
+                    VStack {
+                        Text("Album Favorites")
+                        SquareGridView(items: store.favorites.albumFavorites.elements, columns: 2, columnWidth: width) { album in
+                            if let image = album.images.first?.imageLink {
+                                if let imageUrl = URL(string: image) {
+                                    AsyncThumbnailView(url: imageUrl, width: width)
+                                        .onTapGesture {
+                                            // TODO: Implement Display Albums
+                                        }
+                                }
+                            }
+                        }
+                        Spacer()
+                    }
+                } else {
+                    VStack {
+                        Text("Image Favorites")
+                        SquareGridView(items: store.favorites.imageFavorites.elements, columns: 2, columnWidth: width) { image in
+                            if let imageUrl = URL(string: image.imageLink) {
+                                AsyncThumbnailView(url: imageUrl, width: width)
+                                    .onTapGesture {
+                                        // TODO: Implement Display Albums
+                                    }
+                            }
+                        }
+                    }
+                    Spacer()
+                }
+                Spacer()
+            }
         }
+        
     }
 }
